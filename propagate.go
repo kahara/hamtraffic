f18e6c73ee54d13b596b5ca88df5d102e55ce665
Joni Kähärä <joni.kahara@async.fi>
2023-03-19 00:59:32 +0200

    sleep instead of tickering in propagator

--- propagate.go
+++ propagate.go
@@ -11,17 +11,12 @@ type Timeline struct {
 }
 
 func propagate(backlog <-chan Transmission) {
-
-	ticker := time.NewTicker(time.Duration(1 * time.Millisecond))
-
 	for {
 		select {
 		case transmission := <-backlog:
 			log.Debug().Str("callsign", transmission.Station.Callsign).Float64("frequency", transmission.Frequency).Str("mode", transmission.Mode).Time("timestamp", transmission.Time).Msg("Propagator received a transmission")
 		}
 
-		select {
-		case <-ticker.C:
-		}
+		time.Sleep(time.Duration(1 * time.Millisecond))
 	}
 }
