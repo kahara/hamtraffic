7d1730c9d273c754082756023cded30ac4203861
Joni Kähärä <joni.kahara@async.fi>
2023-03-19 22:38:51 +0200

    propagate, sans "magic"

--- propagate.go
+++ propagate.go
@@ -1,15 +1,28 @@
 package hamtraffic
 
 import (
+	"fmt"
 	"github.com/rs/zerolog/log"
+	"math/rand"
 )
 
 // NOTE We're not attempting to model irl propagation of radio waves
 
-const ()
+const DistanceWiggle = 0.1
 
 func propagate(backlog <-chan Transmission) {
-	var transmission Transmission
+	var (
+		bandmodepairToDistance = func() map[string]float64 {
+			var distances = make(map[string]float64)
+
+			for _, bandmodepair := range config.BandModePairs {
+				distances[fmt.Sprintf("%s@%s", bandmodepair.Band, bandmodepair.Mode)] = 1000000000 // FIXME
+			}
+
+			return distances
+		}()
+		transmission Transmission
+	)
 
 	for {
 		select {
@@ -21,11 +34,24 @@ func propagate(backlog <-chan Transmission) {
 
 		// Compute a base value of how far this transmission will travel, based solely on the hyper-simplification that
 		// transmissions on lower frequencies can be heard farther away than transmissions on higher frequencies
+		baseDistance := bandmodepairToDistance[fmt.Sprintf("%s@%s", transmission.Band, transmission.Mode)]
 
 		// Randomly wiggle the base value
+		baseWiggle := rand.Float64() * DistanceWiggle
+		baseDistance = baseDistance + (-baseWiggle + (baseWiggle * 2.0))
 
 		// Send transmission to each station that is close enough to hear it
+		for d, bin := range transmission.Station.Neighbours {
+			for _, station := range bin {
+				// Wiggle further for each receiving station
+				wiggle := rand.Float64() * DistanceWiggle
+				distance := baseDistance + (-wiggle + (wiggle * 2.0))
 
-		// Wiggle further
+				// Send; receiver decides if it was receiving at the time of transmission
+				if distance >= d {
+					station.Receiver <- transmission
+				}
+			}
+		}
 	}
 }
